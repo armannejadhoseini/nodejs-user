4594e57449b40fe20471a71f61978cd216707e79
armannejadhoseini <68272842+armannejadhoseini@users.noreply.github.com>
2022-12-24 14:33:36 +0330

    Delete src directory

--- src/libs/responseManager.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-class ResponseManager {
-    format(code, message, data) {
-        const response = { code, message, data };
-        return response;
-    }
-}
-module.exports = ResponseManager;
--- src/models/userMethods.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-class UserMethods {
-}
-module.exports = UserMethods;
